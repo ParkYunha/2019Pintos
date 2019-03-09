8658ce981e3cc940139653d21b41e3716337eda6
jiseophan <jshan2017@kaist.ac.kr>
2019-03-09 14:26:51 +0900

    gitignore fixed

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -97,6 +97,7 @@ timer_sleep (int64_t ticks)
 {
   int64_t start = timer_ticks ();
   ASSERT (intr_get_level () == INTR_ON);
+  thread_block();
   while (timer_elapsed (start) < ticks) 
     thread_yield ();
 }
